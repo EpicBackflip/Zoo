3c501c677ebe811380e451e574623c87a61c3733
EpicBackflip <nyla.slot@hva.nl>
2023-12-01 17:33:58 +0100

    cleaning up the code and adding comments and tooltips

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,41 +6,37 @@ namespace Zoo
 {
     public class Animal : MonoBehaviour
     {
+        [Header("required components")]
+        [Tooltip("drag the balloon child object from the chosen animal here")]
+        [SerializeField]
+        private GameObject Balloon;
+        [Tooltip("drag the text child object from the chosen animal here")]
+        [SerializeField]
+        private Text text;
+        
+        [Header("animal attributes")]
+        [Tooltip("here you can set the name animal sound and eating sound for each animal to be displayed based on it's method")]
         public string name;
-        public GameObject Balloon;
-        public Text text;
         [SerializeField]
         private string animalSound;
         [SerializeField]
         private string eatingSound;
+        
+        [Header("animal booleans")]
+        [Tooltip("here you can tick the boxes of the methods you want the animal to be able to execute")]
         [SerializeField]
         private bool canEatMeat;
         [SerializeField]
         private bool canEatLeaves;
         [SerializeField]
         private bool canDoTrick;
-        public Animal(string animalSound, string eatingSound)
-        {
-            this.animalSound = animalSound;
-            this.eatingSound = eatingSound;
-        }
-        
+
+        // these methods simply activate the balloon and set the text to that of the set attributes
+        // theres no need to deactivate the balloons as this is done by a different script
         public void SayHello()
         {
             Balloon.SetActive(true);
             text.text = animalSound;
-        }
-
-        public IEnumerator DoTrick()
-        {
-            for (int i = 0; i < 360; i++)
-            {
-                if (canDoTrick)
-                {
-                    transform.localRotation = Quaternion.Euler(i, 0, 0);
-                    yield return new WaitForEndOfFrame();
-                }
-            }
         }
 
         public void EatMeat()
@@ -61,5 +56,19 @@ namespace Zoo
                 text.text = eatingSound;
             }
         }
+        
+        // this method rotates the local transform of the object until the end of the frame so it  makes a full 360 spin
+        public IEnumerator DoTrick()
+        {
+            for (int i = 0; i < 360; i++)
+            {
+                if (canDoTrick)
+                {
+                    transform.localRotation = Quaternion.Euler(i, 0, 0);
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,12 +6,15 @@ namespace Zoo
 {
     class Spawner : MonoBehaviour
     {
+        //list for refferences of the animals to instantiate 
         [SerializeField]
+        [Tooltip("place a prefab of an animal here to have it spawned when the game starts")]
         private List<Animal> animals = new List<Animal>(7);
-
+        // list of actual animal objects in the game
         private List<Animal> animalobjects = new List<Animal>();
         public InputField InputField;
         
+        //here I instantiate the animals that i've manually put in the list in the inspector
         private void Start()
         {
             for (int i = 0; i < animals.Count; i++)
@@ -19,7 +22,8 @@ namespace Zoo
                 animalobjects.Add( Instantiate(animals[i], transform));
             }
         }
-
+        // these 3 functions all essentially function the same for all the animals in the list they call the eat method or 
+        // the dotrick method
         public void EatMeat()
         {
             foreach (Animal animal in animalobjects)
@@ -34,7 +38,7 @@ namespace Zoo
                 animal.EatLeaves();
             }
         }
-
+        // the only difference here is that it uses a startcoroutine as totrick is a coroutine
         public void DoTrick()
         {
             foreach (Animal animal in animalobjects)
@@ -42,7 +46,7 @@ namespace Zoo
                 animal.StartCoroutine(animal.DoTrick());
             }
         }
-
+        // here I check if inputfield is empty in order for all animals to say hello
         public void SayHello()
         {
             if (InputField.text == "")
@@ -54,6 +58,7 @@ namespace Zoo
             }
             foreach (Animal animal in animalobjects)
             {
+                // here i check if the inputfield text corresponds to an animal name if so that animal says hello
                 if (animal.name == InputField.text)
                 {
                     animal.SayHello();
